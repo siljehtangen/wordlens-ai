e32b3402c25f6423df51d30202423aff1ff44264
Silje Hanø Tangen <siljehtangen@outlook.com>
2026-05-01 15:45:47 +0200

    fix: update state with lens

--- backend/src/state.rs
+++ backend/src/state.rs
@@ -2,12 +2,13 @@ use moka::future::Cache;
 use std::sync::Arc;
 
 use crate::history::History;
+use crate::types::Lens;
 
 #[derive(Clone)]
 pub struct AppState {
     pub http: reqwest::Client,
     pub ollama_generate_url: String,
     pub model: String,
-    pub cache: Cache<(String, String), String>,
+    pub cache: Cache<(String, Lens), String>,
     pub history: Arc<History>,
 }
